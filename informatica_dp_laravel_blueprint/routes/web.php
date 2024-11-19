e356a247799a0b2ef5f9a238ee23697730da8f04
Jose Raul Miranda Prieto <mirandaprietojoseraul@gmail.com>
2024-11-19 06:14:05 -0500

    Keep coding blueprint

--- informatica_dp_laravel_blueprint/routes/web.php
+++ informatica_dp_laravel_blueprint/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('posts', App\Http\Controllers\PostController::class)->only('index', 'store');
+
+
+Route::resource('posts', App\Http\Controllers\PostController::class)->only('index', 'store');
